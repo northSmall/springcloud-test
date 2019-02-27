9e0f4c7329d00994fb13da789fa31d4e76481a18
northSmall <604322962@qq.com>
2019-02-27 23:53:52 +0800

    新增java bean转xml

---
+++ algorithmtest/src/main/java/com/north/News.java
@@ -0,0 +1,41 @@
+package com.north;
+
+public class News {
+    private String Title;//标题
+    private String Description;//描述
+    private String PicUrl;//图片地址
+
+    private String Url;//访问地址
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getPicUrl() {
+        return PicUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        PicUrl = picUrl;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+}
--- algorithmtest/src/main/java/com/north/Wrapper.java
+++ algorithmtest/src/main/java/com/north/Wrapper.java
@@ -3,7 +3,9 @@ package com.north;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 class Wrapper {
@@ -117,11 +119,16 @@ class Wrapper {
 
 
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while ((line = in.readLine()) != null) {
-            TreeNode root = Wrapper.stringToTreeNode(line);
-            Wrapper.prettyPrintTree(root);
-        }
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//        String line;
+//        while ((line = in.readLine()) != null) {
+//            TreeNode root = Wrapper.stringToTreeNode(line);
+//            Wrapper.prettyPrintTree(root);
+//        }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("abc", 333);
+        map.put("abc", 122);
+        System.out.println(map);
     }
 }
